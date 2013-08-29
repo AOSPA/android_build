4f8b733387ed9ca0da61c007d97a677935af6e10
xplodwild <xplod@ouverta.fr>
2013-08-29 20:18:18 +0200

    envsetup: Add pushboot function
    This function lets you push a file from your current OUT dir
    for your current device. It root/remount the device, push the
    file, and reboots it. Example: pushboot system/app/SystemUI.apk
    
    Change-Id: I8a404323a4091e8502fbdd24198c66f1ac598192

--- envsetup.sh
+++ envsetup.sh
@@ -13,6 +13,7 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
 - jgrep:   Greps on all local Java files.
 - resgrep: Greps on all local res/*.xml files.
 - godir:   Go to the directory containing a file.
+- pushboot:Push a file from your OUT dir to your phone and reboots it, using absolute path.
 
 Look at the source to view more functions. The complete list is:
 EOF
@@ -580,6 +581,21 @@ function tapas()
 
     set_stuff_for_environment
     printconfig
+}
+
+function pushboot() {
+    if [ ! -f $OUT/$* ]; then
+        echo "File not found: $OUT/$*"
+        return 1
+    fi
+
+    adb root
+    sleep 1
+    adb wait-for-device
+    adb remount
+
+    adb push $OUT/$* /$*
+    adb reboot
 }
 
 function gettop
