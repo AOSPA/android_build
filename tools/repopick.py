4c8cbb6f01d0231725e19963b31821eecbb2e134
Ayysir <dresadd09691@gmail.com>
2013-11-09 11:48:04 -0500

    repopick.py: Add modified copyright

--- tools/repopick.py
+++ tools/repopick.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #
 # Copyright (C) 2013 The CyanogenMod Project
+# This code has been modified. Portions copyright (C) 2013, ParanoidAndroid Project.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
