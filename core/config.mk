ee99e6df0c9852443b430b9f24efd8211c16a2af
Steve Kondik <shade@chemlab.org>
2013-12-28 15:48:20 -0800

    build: Add QCOM target-specific config
     * Needed to support global DirectTract config on legacy targets
     * Let's also eliminate some boilerplate
    
    Change-Id: I736c10a5e7e1f3d1e0de9e60f29b60add276f151
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>
    
    Conflicts:
    	core/config.mk

--- core/config.mk
+++ core/config.mk
@@ -686,4 +686,7 @@ endif
 RSCOMPAT_32BIT_ONLY_API_LEVELS := 8 9 10 11 12 13 14 15 16 17 18 19 20
 RSCOMPAT_NO_USAGEIO_API_LEVELS := 8 9 10 11 12 13
 
+# Rules for QCOM targets.
+include $(BUILD_SYSTEM)/qcom_target.mk
+
 include $(BUILD_SYSTEM)/dumpvar.mk
