7392c0e852fecfc13ccde1f88ae5b2527a42e8bd
Andre Saddler <dresadd09691@gmail.com>
2014-01-15 19:31:09 -0500

    [2/2] Remove: Movie Studio
    who uses this?
    
    Signed-off-by: Andre Saddler <dresadd09691@gmail.com>
    Change-Id: Ifebb8b327982e2d8e37d443d832247d760d69f96

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2009 The Android Open Source Project
+# This code has been modified. Portions copyright (C) 2014 ParanoidAndroid Project.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,7 +26,6 @@ PRODUCT_PACKAGES := \
     libWnnEngDic \
     libWnnJpnDic \
     libwnndict \
-    VideoEditor \
     WAPPushManager
 
 PRODUCT_PACKAGES += \
