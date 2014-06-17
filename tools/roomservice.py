6eedaea0e44a7abb9490d5e2337a9a2f3cd2f42c
Ryuinferno <ryuinferno.xda@gmail.com>
2014-06-17 04:02:03 +0200

    roomservice: Auto-remove duplicated projects
    roomservice has improved in the sense that once a duplicate path is
    found, a <remove-project> tag will be added into roomservice.xml
    
    Change-Id: If3e1dc672d25996a0cd19628ece24c010e213adf

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -96,10 +96,11 @@ def parse_device_directory(device_url):
 
 
 # Thank you RaYmAn
-def iterate_manifests():
+def iterate_manifests(check_all):
     files = []
-    for file in os.listdir(local_manifest_dir):
-        files.append(os.path.join(local_manifest_dir, file))
+    if check_all:
+        for file in os.listdir(local_manifest_dir):
+            files.append(os.path.join(local_manifest_dir, file))
     files.append('.repo/manifest.xml')
     for file in files:
         try:
@@ -113,10 +114,18 @@ def iterate_manifests():
 
 
 def check_project_exists(url):
-    for project in iterate_manifests():
+    for project in iterate_manifests(True):
         if project.get("name") == url:
             return True
     return False
+
+
+def check_dup_path(directory):
+    for project in iterate_manifests(False):
+        if project.get("path") == directory:
+            print ("Duplicate path %s found! Removing" % directory)
+            return project.get("name")
+    return None
 
 
 # Use the indent function from http://stackoverflow.com/a/4590052
@@ -144,6 +153,12 @@ def create_manifest_project(url, directory,
     if project_exists:
         return None
 
+    dup_path = check_dup_path(directory)
+    if not dup_path is None:
+            write_to_manifest(
+                append_to_manifest(
+                    create_manifest_remove(dup_path)))
+
     project = ES.Element("project",
                          attrib={
                              "path": directory,
@@ -152,6 +167,11 @@ def create_manifest_project(url, directory,
                              "revision": revision
                          })
     return project
+
+
+def create_manifest_remove(url):
+    remove = ES.Element("remove-project", attrib={"name": url})
+    return remove
 
 
 def append_to_manifest(project):
@@ -177,7 +197,7 @@ def write_to_manifest(manifest):
 
 
 def parse_device_from_manifest(device):
-    for project in iterate_manifests():
+    for project in iterate_manifests(True):
         name = project.get('name')
         if name.startswith("android_device_") and name.endswith(device):
             return project.get('path')
