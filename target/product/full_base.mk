2bf9ecf914cc1979123b7b5e94c04600a7061dfc
Evan Anderson <evananderson@aospa.co>
2014-11-05 16:47:18 -0500

    Use the L audio package
    Change-Id: Ib11533e02fda55e1c9715170fac794a44f7886a4
    Signed-off-by: Evan Anderson <evananderson@aospa.co>

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -51,7 +51,7 @@ PRODUCT_LOCALES := en_US
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
 
 # Get some sounds
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage13_48.mk)
 
 # Get the TTS language packs
 $(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
