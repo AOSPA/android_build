309bb9068da525d6e83dd53ab0d4db82cf1258db
ayysir <ayysir@aospal.com>
2014-11-08 02:43:19 -0500

    tools: Roomservice
    (squashed)
    
    History
    ===
    
    [*]roomservice: Auto-remove duplicated projects
    
    roomservice has improved in the sense that once a duplicate path is
    found, a <remove-project> tag will be added into roomservice.xml
    
    Change-Id: If3e1dc672d25996a0cd19628ece24c010e213adf
    
    [*]roomservice: Include forked repositories in device search
    
    The github api defaults to not including forked repos in search results.
    This causes roomservice to fail to find a device repo if it is a fork (I.E. maguro).
    This commit adds the necessary argument to include forked repositories in the results
    and fixes maguro syncing.
    
    Change-Id: I50ec96316c3208e977d659a1dd90205a2b3d85b3
    Signed-off-by: Evan Anderson <evan1124@gmail.com>
    
    [*]roomservice: Fix device syncing
    
    Change-Id: Id4a59d74531a4ea1744f845fbe65b43debf950dd
    Signed-off-by: Evan Anderson <evan1124@gmail.com>
    
    [*]Switch to OmniRom's roomservice
    
    All credit goes to Omni for this.
    
    PS2: Update defaults to use with PA
    PS3: Give proper authorship
    PS4: Update dependency file name
    PS5: Set default_team_rem to github
    PS6: Revert PS5. Depends on manifest change
    
    Change-Id: I77fe0e958a3da5a3d65d0bf8c929fed2b13115bd
    Signed-off-by: Evan Anderson <evan1124@gmail.com>
    
    [*]roomservice: fix missing revision line
    
    Change-Id: I727f3a6ab1345b32b61b1c1bda31f03008082c96
    Signed-off-by: Evan Anderson <evan1124@gmail.com>
    
    [*]roomservice: Fix adding to manifest
    
    Python is indent-sensitive, so needed to indent some lines.
    
    Change-Id: Iaa360754038e4966fb76eb264e7c3195209735a1
    Signed-off-by: Evan Anderson <evan1124@gmail.com>
    
    Contributers:
    @Ryuinferno
    @Cybojenix
    
    Change-Id: I91ee64727edf80cbe2cce7934de0adcf813b55a6

--- envsetup.sh
+++ envsetup.sh
@@ -511,6 +511,18 @@ function lunch()
     check_product $product
     if [ $? -ne 0 ]
     then
+        # if we can't find a product, try to grab it off the CM github
+        T=$(gettop)
+        pushd $T > /dev/null
+        build/tools/roomservice.py $product
+        popd > /dev/null
+        check_product $product
+    else
+        build/tools/roomservice.py $product true
+    fi
+
+    if [ $? -ne 0 ]
+    then
         echo
         echo "** Don't have a product spec for: '$product'"
         echo "** Do you have the right repo manifest?"
