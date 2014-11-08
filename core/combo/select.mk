5531da3e83567d442a08eb15bffb143db9c82b19
ayysir <ayysir@aospal.com>
2014-11-08 02:47:03 -0500

    5.0 Bring up
    - Build CellBroadcastReceiver
    - Replace Launcher2 w/ Launcher3 & remove QuickSearchBox
    - add PRODUCT_BUILD_PROP_OVERRIDES
    - Remove deprecated Music package
    - Remove optional "Home Sample" Launcher
    - Build Stk (Sim Toolkit)
    - Turn off data roaming by defualt
    - envsetup: remove cruft
    - envsetup: envsetup: add a helper to fix "out/target/common" not being really common (http://goo.gl/Yadnwq)
    - ccache: Use system version over prebuilt (http://goo.gl/qcF3ML)
    - envsetup: set OUT_DIR to an absolute path always (http://goo.gl/y0q93m) & (http://goo.gl/Gs7lBc)
    - Allow cache.img to be disabled (http://goo.gl/ggtU6t)
    - Fix file-size stats on OSX (http://goo.gl/hMcPFC)
    - core: main: remove all the spam about including make files (http://goo.gl/t3HdQv)
    - enable shared ccache (http://goo.gl/JScBO9) & (http://goo.gl/PpBBZb)
    - disable openjdk check for Linux Host OS
    
    Change-Id: I3b0c6b0ab1be0f33f32d6ee9be63140ec2252ec7

--- core/combo/select.mk
+++ core/combo/select.mk
@@ -80,7 +80,9 @@ ifneq ($(USE_CCACHE),)
   # We don't really use system headers much so the rootdir is
   # fine; ensures these paths are relative for all Android trees
   # on a workstation.
-  export CCACHE_BASEDIR := /
+  ifeq ($(CCACHE_BASEDIR),)
+    export CCACHE_BASEDIR := $(ANDROID_BUILD_TOP)
+  endif
 
   # Workaround for ccache with clang.
   # See http://petereisentraut.blogspot.com/2011/09/ccache-and-clang-part-2.html
@@ -92,7 +94,16 @@ ifneq ($(USE_CCACHE),)
   ifeq ($(HOST_OS)-$(BUILD_OS),windows-linux)
     CCACHE_HOST_TAG := linux-$(HOST_PREBUILT_ARCH)
   endif
-  ccache := prebuilts/misc/$(CCACHE_HOST_TAG)/ccache/ccache
+
+  # Check if there is a ccache binary provided by the system
+  # If there is, use it instead of prebuilt binary
+  ccache_version := $(shell ccache --version 2>/dev/null)
+  ifdef ccache_version
+    ccache := $(shell which ccache)
+  else
+    ccache := prebuilts/misc/$(CCACHE_HOST_TAG)/ccache/ccache
+  endif
+
   # Check that the executable is here.
   ccache := $(strip $(wildcard $(ccache)))
   ifdef ccache
