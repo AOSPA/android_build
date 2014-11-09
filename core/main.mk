9ea9ee328e8a17b71fc2dfb6fecb664de060838d
SferaDev <sferadev@gmail.com>
2014-11-09 11:03:41 +0000

    build: Don't stop if JDK version doesn't match
    Temporal change, right now unofficial OpenJDK 1.7 builds error out.
    
    Change-Id: I5e77c70724c7d81544673715ed09e0a53ce32730

--- core/main.mk
+++ core/main.mk
@@ -207,7 +207,6 @@ $(info $(space))
 $(info Please follow the machine setup instructions at)
 $(info $(space)$(space)$(space)$(space)https://source.android.com/source/initializing.html)
 $(info ************************************************************)
-$(error stop)
 endif
 
 # Check for the current JDK.
@@ -228,7 +227,6 @@ $(info ************************************************************)
 $(info You asked for an OpenJDK based build but your version is)
 $(info $(java_version_str).)
 $(info ************************************************************)
-$(error stop)
 endif # java version is not OpenJdk
 else # if requires_openjdk
 ifneq ($(shell echo '$(java_version_str)' | grep -i openjdk),)
@@ -239,7 +237,6 @@ $(info You use OpenJDK but only Sun/Oracle JDK is supported.)
 $(info Please follow the machine setup instructions at)
 $(info $(space)$(space)$(space)$(space)https://source.android.com/source/download.html)
 $(info ************************************************************)
-$(error stop)
 endif # java version is not Sun Oracle JDK
 endif # if requires_openjdk
 
@@ -266,7 +263,6 @@ $(info $(space))
 $(info Please follow the machine setup instructions at)
 $(info $(space)$(space)$(space)$(space)https://source.android.com/source/download.html)
 $(info ************************************************************)
-$(error stop)
 endif
 
 endif # if JAVA_NOT_REQUIRED
