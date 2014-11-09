576fd41070718ad7598b48406aec2f8a71b8b49d
SferaDev <sferadev@gmail.com>
2014-11-09 11:03:41 +0000

    Don't stop if JDK version doesn't match
    Change-Id: I5e77c70724c7d81544673715ed09e0a53ce32730
    Signed-off-by: Louis Popi <theh2o64@gmail.com>

--- core/main.mk
+++ core/main.mk
@@ -189,7 +189,6 @@ $(info ************************************************************)
 $(info You asked for an OpenJDK 7 build but your version is)
 $(info $(java_version_str).)
 $(info ************************************************************)
-$(error stop)
 endif # java version is not OpenJdk
 else # if requires_openjdk
 ifneq ($(shell echo '$(java_version_str)' | grep -i openjdk),)
@@ -200,7 +199,6 @@ $(info You use OpenJDK but only Sun/Oracle JDK is supported.)
 $(info Please follow the machine setup instructions at)
 $(info $(space)$(space)$(space)$(space)https://source.android.com/source/download.html)
 $(info ************************************************************)
-$(error stop)
 endif # java version is not Sun Oracle JDK
 endif # if requires_openjdk
 
@@ -216,7 +214,6 @@ $(info $(space))
 $(info Please follow the machine setup instructions at)
 $(info $(space)$(space)$(space)$(space)https://source.android.com/source/download.html)
 $(info ************************************************************)
-$(error stop)
 endif
 
 
