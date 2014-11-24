5aa4b3c35156e64ac254c0c28886f835df0972ee
Evan Anderson <evananderson@aospa.co>
2014-11-24 00:18:05 -0500

    core/main.mk: Allow OpenJDK
    OpenJDK is an equivilent implementation of java and
    will produce the same results as oracle JDK
    
    Signed-off-by: Evan Anderson <evananderson@aospa.co>
    Change-Id: I4da51ce9bbd1988777a2d887605482852609c6ba

--- core/main.mk
+++ core/main.mk
@@ -188,16 +188,6 @@ $(info You asked for an OpenJDK 7 build but your version is)
 $(info $(java_version_str).)
 $(info ************************************************************)
 endif # java version is not OpenJdk
-else # if requires_openjdk
-ifneq ($(shell echo '$(java_version_str)' | grep -i openjdk),)
-$(info ************************************************************)
-$(info You are attempting to build with an unsupported JDK.)
-$(info $(space))
-$(info You use OpenJDK but only Sun/Oracle JDK is supported.)
-$(info Please follow the machine setup instructions at)
-$(info $(space)$(space)$(space)$(space)https://source.android.com/source/download.html)
-$(info ************************************************************)
-endif # java version is not Sun Oracle JDK
 endif # if requires_openjdk
 
 # Check for the correct version of javac
