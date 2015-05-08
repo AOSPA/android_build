53eb31abc704906df0d5371af067a507dc7a9f5d
Adnan Begovic <adnan@cyngn.com>
2015-05-08 14:42:25 -0700

    build/core: Create means of ignoring subdir layer for packages.
      An external resource package that acts as a secondary framework
      resource should be presented in system/framework similarily to
      the framework-res module.
    
    Change-Id: Ie4110a184cd7262035110a6a04bb7ea91e7a42b0

--- core/base_rules.mk
+++ core/base_rules.mk
@@ -265,7 +265,7 @@ ifneq (true,$(LOCAL_UNINSTALLABLE_MODULE))
   # Apk and its attachments reside in its own subdir.
   ifeq ($(LOCAL_MODULE_CLASS),APPS)
   # framework-res.apk doesn't like the additional layer.
-  ifeq ($(LOCAL_NO_STANDARD_LIBRARIES),true)
+  ifneq ($(filter true,$(LOCAL_NO_STANDARD_LIBRARIES) $(LOCAL_IGNORE_SUBDIR)),)
   # Neither do Runtime Resource Overlay apks, which contain just the overlaid resources.
   else ifeq ($(LOCAL_IS_RUNTIME_RESOURCE_OVERLAY),true)
   else
