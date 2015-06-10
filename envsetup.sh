6ae7baa0e2e7f94573451e02a1299db7a3323926
Matt Mower <mowerm@gmail.com>
2015-06-09 19:35:53 -0500

    build: Enable custom ccache cache dir for Android
    During lunch, check environment variables for ANDROID_CCACHE_DIR.
    If it is specified, CCACHE_DIR is set to match. Prevents issues
    with multiple versions of ccache writing to the same cache dir.
    
    ANDROID_CCACHE_SIZE should also be specified, allowing for a distinct
    cache directory size from the default. Syntax for ANDROID_CCACHE_SIZE
    is the same as applied in the command: 'ccache -M$ANDROID_CCACHE_SIZE'
    
    Change-Id: I17497a6f56347850e0fa7b8ebd4de0b5a0b13e55
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -270,6 +270,10 @@ function setpaths()
     unset ANDROID_TARGET_OUT_TESTCASES
     export ANDROID_TARGET_OUT_TESTCASES=$(get_abs_build_var TARGET_OUT_TESTCASES)
 
+    if [ -n "$ANDROID_CCACHE_DIR" ]; then
+        export CCACHE_DIR=$ANDROID_CCACHE_DIR
+    fi
+
     # needed for building linux on MacOS
     # TODO: fix the path
     #export HOST_EXTRACFLAGS="-I "$T/system/kernel_headers/host_include
