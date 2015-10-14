96a55592c17e68176aa39dc59b09dd01354b345d
Alexis Rico <me@sferadev.com>
2015-10-14 20:11:34 +0000

    build: Add mka
    Derived from:
    https://github.com/CyanogenMod/android_build/commit/d6bd065aec2307938bd03507c5ec6be3295bcbb4
    https://github.com/CyanogenMod/android_build/commit/3b90994fae30df4ed40d12d2cb55e63ed9e4cde8
    
    Change-Id: I8f91670adb2a7d93f991b129cf2745d058305d5c

--- envsetup.sh
+++ envsetup.sh
@@ -26,6 +26,7 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
 - sepgrep:   Greps on all local sepolicy files.
 - sgrep:     Greps on all local source files.
 - godir:     Go to the directory containing a file.
+- mka:     Builds using SCHED_BATCH on all processors
 
 Environment options:
 - SANITIZE_HOST: Set to 'true' to use ASAN for all host modules. Note that
@@ -1597,6 +1598,17 @@ function godir () {
         pathname=${lines[0]}
     fi
     \cd $T/$pathname
+}
+
+function mka() {
+    case `uname -s` in
+        Darwin)
+            make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
+            ;;
+        *)
+            schedtool -B -n 1 -e ionice -n 1 make -j$(cat /proc/cpuinfo | grep "^processor" | wc -l) "$@"
+            ;;
+    esac
 }
 
 # Print colored exit condition
