fa787ac3f6c08851509698e96603c6ec5286fd69
Alexis Rico <me@sferadev.com>
2015-11-11 17:14:18 +0000

    build: Roomservice for Paranoid Android
    - Remove the device search/sync
    - Use vendor/pa to host dependencies files
    - Remvoe GitHub dependency
    - Clean up comments and code
    - Refactor error/warning messages
    - Use --force-sync instead of -f
    
    Change-Id: Ic68c91d467c23d0f47fac4d8a6e8d56f6ddb0993

--- envsetup.sh
+++ envsetup.sh
@@ -623,14 +623,11 @@ function lunch()
     check_variant $variant
     if [ $? -ne 0 ]
     then
-        # if we can't find a product, try to grab it off github
         T=$(gettop)
         pushd $T > /dev/null
         build/tools/roomservice.py $product
         popd > /dev/null
         check_product $product
-    else
-        build/tools/roomservice.py $product true
     fi
 
     if [ $? -ne 0 ]
