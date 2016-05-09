f2aa8ec61d9e1ca9b68082c0dc1a7db9590653fc
luca020400 <luca.stefani.ge1@gmail.com>
2016-05-09 15:16:19 +0200

    build: Move to in-build DTB directory search
    * We can't depend on a $OUT variable with ninja.
    
    Change-Id: I9ff050a986e7bb2774d1326f5b396d0afb7f33c1
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- core/tasks/generate_extra_images.mk
+++ core/tasks/generate_extra_images.mk
@@ -36,11 +36,16 @@ DTBTOOL := $(HOST_OUT_EXECUTABLES)/$(DTBTOOL_NAME)$(HOST_EXECUTABLE_SUFFIX)
 INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
 
 possible_dtb_dirs = $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/ $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/
-dtb_dir = $(firstword $(wildcard $(possible_dtb_dirs)))
 
 define build-dtimage-target
     $(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
-    $(hide) $(DTBTOOL) -o $@ -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(dtb_dir)
+    $(hide) for dir in $(possible_dtb_dirs); do \
+        if [ -d "$$dir" ]; then \
+            dtb_dir="$$dir"; \
+            break; \
+        fi; \
+    done; \
+    $(DTBTOOL) -o $@ -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ "$$dtb_dir";
     $(hide) chmod a+r $@
 endef
 
