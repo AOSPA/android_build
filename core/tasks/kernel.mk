e7c0c8b0ba8cf542c418e05e794ef1ab63901623
Willi Ye <williye97@gmail.com>
2016-06-09 13:15:34 +0200

    kernel: Fix aarch64 toolchain on darwin
    Change-Id: Ie0625c5c10818e3e679d3b7222ee8d633c9bfc9e

--- core/tasks/kernel.mk
+++ core/tasks/kernel.mk
@@ -34,7 +34,7 @@ endif
 endif
 
 ifeq ($(KERNEL_ARCH),arm64)
-KERNEL_CROSS_COMPILE := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-$(HOST_OS)-android-4.9/bin/aarch64-$(HOST_OS)-android-
+KERNEL_CROSS_COMPILE := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 else ifeq ($(KERNEL_ARCH),arm)
 KERNEL_CROSS_COMPILE := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin/arm-eabi-
 else
