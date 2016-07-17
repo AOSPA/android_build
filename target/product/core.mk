e63d0a657145994781b6515d58712827b000724f
Evan Anderson <evananderson@aospa.co>
2016-07-17 15:50:38 -0500

    core: Remove QuickSearchBox
    Nobody wants to see this ugly monstrosity
    
    Change-Id: I7085c71461bfc8810c499ae75fbbcdfa806d0711
    Signed-off-by: Evan Anderson <evananderson@aospa.co>

--- target/product/core.mk
+++ target/product/core.mk
@@ -52,7 +52,6 @@ PRODUCT_PACKAGES += \
     PrintSpooler \
     PrintRecommendationService \
     ProxyHandler \
-    QuickSearchBox \
     Settings \
     SharedStorageBackup \
     StorageManager \
