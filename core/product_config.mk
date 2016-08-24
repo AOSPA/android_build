21496a9db0f47e30a7abf2aaee50e8479e3bc8e8
Alexis Rico <sferadev@gmail.com>
2016-08-24 14:39:21 +0200

    build: Fetch product makefiles from vendor/pa
    Change-Id: If1eb3a7e6638286218c6e07015186d94cde89b34

--- core/product_config.mk
+++ core/product_config.mk
@@ -184,9 +184,13 @@ ifneq ($(strip $(TARGET_BUILD_APPS)),)
 all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
-# Read in all of the product definitions specified by the AndroidProducts.mk
-# files in the tree.
-all_product_configs := $(get-all-product-makefiles)
+  ifdef PA_VERSION
+    all_product_configs := $(shell ls vendor/pa/*/$(TARGET_PRODUCT).mk)
+  else
+    # Read in all of the product definitions specified by the AndroidProducts.mk
+    # files in the tree.
+    all_product_configs := $(get-all-product-makefiles)
+  endif #PA_VERSION
 endif
 
 # Find the product config makefile for the current product.
