89901d27ed4fbca56ab7ba2ad0f9db4e1ac57ee7
Jake Weinstein <xboxlover360@gmail.com>
2016-08-30 17:00:43 -0400

    build: remove unnecessary lunch items
    Change-Id: I400f31e2f012fd6f71d1ec2728e9187730d24d20

--- envsetup.sh
+++ envsetup.sh
@@ -552,14 +552,6 @@ function add_lunch_combo()
     LUNCH_MENU_CHOICES=(${LUNCH_MENU_CHOICES[@]} $new_combo)
 }
 
-# add the default one here
-add_lunch_combo aosp_arm-eng
-add_lunch_combo aosp_arm64-eng
-add_lunch_combo aosp_mips-eng
-add_lunch_combo aosp_mips64-eng
-add_lunch_combo aosp_x86-eng
-add_lunch_combo aosp_x86_64-eng
-
 function print_lunch_menu()
 {
     local uname=$(uname)
