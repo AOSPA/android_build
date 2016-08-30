ab81614a4184b3747687ea2f4f031984b17a990d
Jake Weinstein <xboxlover360@gmail.com>
2016-08-30 17:00:43 -0400

    build: remove unnecessary lunch items
    Change-Id: I400f31e2f012fd6f71d1ec2728e9187730d24d20
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- target/product/AndroidProducts.mk
+++ target/product/AndroidProducts.mk
@@ -78,9 +78,3 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/module_arm64.mk \
     $(LOCAL_DIR)/module_x86.mk \
     $(LOCAL_DIR)/module_x86_64.mk \
-
-COMMON_LUNCH_CHOICES := \
-    aosp_arm64-eng \
-    aosp_arm-eng \
-    aosp_x86_64-eng \
-    aosp_x86-eng \
