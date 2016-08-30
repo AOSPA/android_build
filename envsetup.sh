b3ccedf6c805ac77bfcd6494b6025384d3b4f96a
Jake Weinstein <xboxlover360@gmail.com>
2016-08-30 17:00:43 -0400

    build: remove unnecessary lunch items
    Change-Id: I400f31e2f012fd6f71d1ec2728e9187730d24d20

--- envsetup.sh
+++ envsetup.sh
@@ -452,7 +452,7 @@ function lunch()
         answer=$1
     else
         print_lunch_menu
-        echo "Which would you like? [aosp_cf_x86_64_phone-trunk_staging-eng]"
+        echo "Which would you like?"
         echo -n "Pick from common choices above (e.g. 13) or specify your own (e.g. aosp_barbet-trunk_staging-eng): "
         read answer
         used_lunch_menu=1
@@ -462,7 +462,8 @@ function lunch()
 
     if [ -z "$answer" ]
     then
-        selection=aosp_cf_x86_64_phone-trunk_staging-eng
+        echo -n "\nI didn't understand your response. Please try again.\n"
+        lunch
     elif (echo -n $answer | grep -q -e "^[0-9][0-9]*$")
     then
         local choices=($(TARGET_BUILD_APPS= TARGET_PRODUCT= TARGET_RELEASE= TARGET_BUILD_VARIANT= _get_build_var_cached COMMON_LUNCH_CHOICES 2>/dev/null))
