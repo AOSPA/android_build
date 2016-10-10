eeee3f34135ea2acd2f4c0271ebc2cba8b316d01
Alex Naidis <alex.naidis@linux.com>
2016-10-10 16:20:00 +0200

    Death Extensions
    Change-Id: Iafdb521222cc4fb9d67d94d3eb3a90eca1a23fee
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- core/combo/fdo.mk
+++ core/combo/fdo.mk
@@ -20,8 +20,8 @@ $(combo_2nd_arch_prefix)TARGET_FDO_CFLAGS:=
 
 # Set BUILD_FDO_INSTRUMENT=true to turn on FDO instrumentation.
 # The profile will be generated on /sdcard/fdo_profile on the device.
-$(combo_2nd_arch_prefix)TARGET_FDO_INSTRUMENT_CFLAGS := -fprofile-generate=/sdcard/fdo_profile -DANDROID_FDO
-$(combo_2nd_arch_prefix)TARGET_FDO_INSTRUMENT_LDFLAGS := -lgcov -lgcc
+$(combo_2nd_arch_prefix)TARGET_FDO_INSTRUMENT_CFLAGS := -fprofile-generate=/sdcard/fdo_profile --coverage -DANDROID_FDO
+$(combo_2nd_arch_prefix)TARGET_FDO_INSTRUMENT_LDFLAGS := --coverage
 
 # Set TARGET_FDO_PROFILE_PATH to set a custom profile directory for your build.
 ifeq ($(strip $($(combo_2nd_arch_prefix)TARGET_FDO_PROFILE_PATH)),)
