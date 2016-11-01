262f8423d3ad6777d477b8d8e14a161203810b11
Alex Naidis <alex.naidis@linux.com>
2016-11-01 14:39:16 +0100

    arm64: Rework A53 Errata Fixing logic
    This commit introduces an internal variable
    "APPLY_A53_ERRATA_FIXES" which will be the final
    indicator on whether to apply the Errata fixes
    for Cortex-A53 or not. Since the variable is internal
    only, it gets cleared before and after usage.
    
    Additionally to the CPU detection logic, we are
    now reading "TARGET_IS_CORTEX-A53" which is a flag
    that CAF sets to true on chipsets which contain
    a Cortex-A53, even if they are using "generic"
    as their CPU target. So this is the only way
    to find out whether an Cortex-A53 is really
    present.
    
    We are overriding any results from the CPU
    target detection with what comes from
    "TARGET_IS_CORTEX-A53" if it is present now.
    
    That means that, we are also introducing a
    way to force opting in and force opting out of
    the Cortex-A53 Errata fixes.
    
    Additionally the CPU detection logic
    is deduplicated and cleaned up.
    
    Change-Id: I1f46cdc41087c4b45c365211be5e9ef0bbdc97f1
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- core/combo/arch/arm64/armv8-a.mk
+++ core/combo/arch/arm64/armv8-a.mk
@@ -1,14 +1,21 @@
+APPLY_A53_ERRATA_FIXES :=
+
 ifneq (,$(filter kryo,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
 	arch_variant_cflags := -mcpu=cortex-a57
 else
 ifneq (,$(filter cortex-a53,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
 	arch_variant_cflags := -mcpu=cortex-a53
+	APPLY_A53_ERRATA_FIXES := true
 else
 	arch_variant_cflags :=
 endif
 endif
 
-ifneq (,$(filter cortex-a53 default,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+ifneq ($(strip $(TARGET_IS_CORTEX-A53)),)
+	APPLY_A53_ERRATA_FIXES := $(TARGET_IS_CORTEX-A53)
+endif
+
+ifeq ($(APPLY_A53_ERRATA_FIXES),true)
 	arch_variant_cflags  += -mfix-cortex-a53-835769
 	arch_variant_ldflags := -Wl,--fix-cortex-a53-843419
 	arch_variant_ldflags += -Wl,--fix-cortex-a53-835769
@@ -19,3 +26,4 @@ else
 	RS_DISABLE_A53_WORKAROUND := true
 endif
 
+APPLY_A53_ERRATA_FIXES :=
