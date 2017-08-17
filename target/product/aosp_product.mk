6478e83b4fb992c5a1b589c2497b51901529265f
Luca Stefani <luca020400@lineageos.org>
2017-08-17 21:21:23 +0200

    build: Add AOSPA build support
    Squashed also with the following changes:
    
      Author: dianlujitao <dianlujitao@lineageos.org>
      Date:   Thu Sep 5 21:56:36 2019 +0800
    
        product: Exclude AOSP apn list on AOSPA build
    
        Change-Id: I1fd10089421a2e4b57ef565d87ad1f6036bd1898
    
      Author: dhacker29 <dhackerdvm@gmail.com>
      Date:   Sat Jun 6 22:22:05 2015 -0400
    
        Remove LatinIME as it is included per target in vendor/aospa
    
        Change-Id: Ibce4a244ec0653a7c7798d864047ac667866a1dc
    
      Author: razorloves <razorloves@gmail.com>
      Date:   Mon Oct 7 05:10:56 2019 -0500
    
        build: Remove su inclusion
    
        This stops including su in all AOSPA userdebug/eng builds.
        Inclusion of su is conditionally handled in /vendor/aospa.
    
        Change-Id: Ia3ad978146f170a2b260d77afe9eb580d4b4f823
    
    Change-Id: Iab8b2ef8b644a7bea10bfd28b99d9e69539b1357
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -19,7 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
 # Default AOSP sounds
+ifeq ($(AOSPA_BUILD),)
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+endif
 
 # Additional settings used in all AOSP builds
 PRODUCT_PRODUCT_PROPERTIES += \
@@ -43,5 +45,7 @@ endif #TARGET_IS_QLMD
 
 # Telephony:
 #   Provide a APN configuration to GSI product
+ifeq ($(AOSPA_BUILD),)
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif
