c1765d3b171a144a96da5694d572fb0686a0d44c
joshuous <joshuous@gmail.com>
2017-10-01 03:04:47 +0000

    build: Export SDClang environment variables
    * Soong complains when we try to export the variables via Makefiles
    
    Change-Id: Ie847feb733420a45161eef596ea970c350eb447e
    Signed-off-by: joshuous <joshuous@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -295,11 +295,19 @@ function set_stuff_for_environment()
     set_java_home
     setpaths
     set_sequence_number
+    set_sdclang
 
     export ANDROID_BUILD_TOP=$(gettop)
     # With this environment variable new GCC can apply colors to warnings/errors
     export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
     export ASAN_OPTIONS=detect_leaks=0
+}
+
+function set_sdclang()
+{
+    export SDCLANG_AE_CONFIG=device/qcomm/common/sdclangAE.json
+    export SDCLANG_CONFIG=device/qcom/common/sdclang.json
+    export SDCLANG_SA_ENABLED=false
 }
 
 function set_sequence_number()
