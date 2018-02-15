8ce6f0c8d993cc51bece3ba4a1575e9357588e84
Paul Keith <javelinanddart@gmail.com>
2018-02-15 21:46:00 +0100

    soong: Add var for linker shim libs
    * To limit security exposure, we're forcing all devices
      to define shim libs with a TARGET_LD_SHIM_LIBS
    
    Change-Id: I4348b96dcc5a34c91b1b4315ebf3dc248e6e4b7e

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -92,9 +92,11 @@ $(SOONG_VARIABLES): FORCE
 	echo '    "DeviceKernelHeaders": $(call json_list,$(strip $(TARGET_PROJECT_SYSTEM_INCLUDES))),'; \
 	echo ''; \
 	echo '    "BoardUsesQTIHardware":  $(if $(BOARD_USES_QTI_HARDWARE),true,false),';  \
-        echo ''; \
+	echo ''; \
 	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true),'; \
-	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)"'; \
+	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)",'; \
+	echo ''; \
+	echo '    "Target_shim_libs": "$(TARGET_LD_SHIM_LIBS)"'; \
 	echo '}') > $(SOONG_VARIABLES_TMP); \
 	if ! cmp -s $(SOONG_VARIABLES_TMP) $(SOONG_VARIABLES); then \
 	  mv $(SOONG_VARIABLES_TMP) $(SOONG_VARIABLES); \
