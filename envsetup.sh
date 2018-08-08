adc746301278f9ddc7f8a4b5c4401fec30cf7acb
David Ng <dave@codeaurora.org>
2018-08-07 19:36:41 -0700

    Relocate vendor_hal hidl-gen logic; improve incremental build
    Switch vendor_hal hidl-gen script to new location (backward
    compatible during transition period).  The relocated script also
    has a new optional --check to see if hidl-gen source or
    vendor interfaces have changed.  If there are no changes, skip
    the unnecessary hidl-gen ALLOW_MISSING_DEPENDENCIES compilation
    to improve incremental build time.
    
    CRs-Fixed: 2294306
    Change-Id: I8679ec13f01bc3733ec8b3599f8c60d6fffaa8f5

--- envsetup.sh
+++ envsetup.sh
@@ -1666,19 +1666,30 @@ function make()
         return 1
     fi
 
-    _wrap_build $(get_make_command hidl-gen) hidl-gen ALLOW_MISSING_DEPENDENCIES=true
-    RET=$?
-    if [ $RET -ne 0 ]; then
-        echo -n "${color_failed}#### hidl-gen compilation failed, check above errors"
-        echo " ####${color_reset}"
-        return $RET
+    if [ -f $ANDROID_BUILD_TOP/$QTI_BUILDTOOLS_DIR/build/update-vendor-hal-makefiles.sh ]; then
+        vendor_hal_script=$ANDROID_BUILD_TOP/$QTI_BUILDTOOLS_DIR/build/update-vendor-hal-makefiles.sh
+        source $vendor_hal_script --check
+        regen_needed=$?
+    else
+        vendor_hal_script=$ANDROID_BUILD_TOP/device/qcom/common/vendor_hal_makefile_generator.sh
+        regen_needed=1
     fi
-    source $ANDROID_BUILD_TOP/device/qcom/common/vendor_hal_makefile_generator.sh
-    RET=$?
-    if [ $RET -ne 0 ]; then
-        echo -n "${color_failed}#### HAL file .bp generation failed dure to incpomaptible HAL files , please check above error log"
-        echo " ####${color_reset}"
-        return $RET
+
+    if [ $regen_needed -eq 1 ]; then
+        _wrap_build $(get_make_command hidl-gen) hidl-gen ALLOW_MISSING_DEPENDENCIES=true
+        RET=$?
+        if [ $RET -ne 0 ]; then
+            echo -n "${color_failed}#### hidl-gen compilation failed, check above errors"
+            echo " ####${color_reset}"
+            return $RET
+        fi
+        source $vendor_hal_script
+        RET=$?
+        if [ $RET -ne 0 ]; then
+            echo -n "${color_failed}#### HAL file .bp generation failed dure to incpomaptible HAL files , please check above error log"
+            echo " ####${color_reset}"
+            return $RET
+        fi
     fi
     _wrap_build $(get_make_command "$@") "$@"
 }
