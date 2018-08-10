f470fccbfd4a0840788bb43cc42be68b4bd7fbbb
Simon Shields <simon@lineageos.org>
2018-08-10 14:39:32 +1000

    soong: include AOSPA soong_config
    Change-Id: Ic7b8993f4a201f8a9d24c6ba32617fa34843afc2

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -179,6 +179,8 @@ $(foreach namespace,$(SOONG_CONFIG_NAMESPACES),\
   $(call end_json_map))
 $(call end_json_map)
 
+include vendor/pa/build/soong/soong_config.mk
+
 $(call json_end)
 
 $(file >$(SOONG_VARIABLES).tmp,$(json_contents))
