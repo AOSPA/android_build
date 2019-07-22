8351eb58f63a5e3b8b8c3025bbf4f22029bad9ad
TheStrix <parthbhatia98@gmail.com>
2019-07-22 20:10:52 +0530

    build: Include pa/sepolicy
    Change-Id: I3f4971da67478c0438a6c9175f91c078a3436b6a

--- core/config.mk
+++ core/config.mk
@@ -1085,4 +1085,10 @@ include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
 endif
 
+ifneq ($(PA_BUILD),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include device/pa/sepolicy/common/sepolicy.mk)
+endif
+
 include $(BUILD_SYSTEM)/dumpvar.mk
