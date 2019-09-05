0e1228fdc9a1ac1205cbd1037717e552c7774a2a
dianlujitao <dianlujitao@lineageos.org>
2019-09-05 21:56:36 +0800

    product: Exclude AOSP APN list on AOSPA builds.
    Change-Id: I1fd10089421a2e4b57ef565d87ad1f6036bd1898

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -37,5 +37,7 @@ PRODUCT_PACKAGES += \
 
 # Telephony:
 #   Provide a APN configuration to GSI product
+ifeq ($(AOSPA_BUILD),)
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif
