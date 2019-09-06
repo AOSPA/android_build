36119d9cbe5ad6dacfece9ff9891cb50fd74ef61
TheStrix <parthbhatia98@gmail.com>
2019-09-06 08:15:07 +0000

    build: Include our BoardConfigPA.mk from vendor
    Change-Id: I71860d0902d348b10433b7e66cdb24bff282f3a4

--- core/config.mk
+++ core/config.mk
@@ -224,6 +224,10 @@ include $(BUILD_SYSTEM)/envsetup.mk
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
+ifneq ($(PA_BUILD),)
+include vendor/pa/config/BoardConfigPA.mk
+endif
+
 # The build system exposes several variables for where to find the kernel
 # headers:
 #   TARGET_DEVICE_KERNEL_HEADERS is automatically created for the current
