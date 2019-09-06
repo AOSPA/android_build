5e4c323036bd6bf55879df5892740a9c0d54a704
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-05 21:35:38 -0700

    core: Only include PA SE policy if repo exists
    Change-Id: If799106d2da6d8c303430e316edff365a22f7c99

--- core/config.mk
+++ core/config.mk
@@ -1184,9 +1184,11 @@ DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
 
 ifneq ($(PA_BUILD),)
+ifneq ($(wildcard device/pa/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
 $(eval include device/pa/sepolicy/common/sepolicy.mk)
+endif
 endif
 
 # Include any vendor specific config.mk file
