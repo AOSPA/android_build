1aaa5c92d2c2a385d9f4f85d271113c989ad8130
Chris Crump <chris@cypheros.co>
2019-09-17 03:27:15 -0400

    product: Generate custom audio on PA builds
    When building PA, a custom audio set (Ringtones, Alarms, etc.) will be used instead of the default AOSP ones
    
    Change-Id: Ia2f0506bb3d24d8ce9705929e97e227345101e80

--- target/product/gsi_common.mk
+++ target/product/gsi_common.mk
@@ -20,13 +20,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/mainline_system.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
-# Default AOSP sounds
+# Default system sounds
+ifeq ($(PA_BUILD),)
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+else
+$(call inherit-product-if-exists, vendor/pa/prebuilt/audio/ParanoidAudio.mk)
+endif
 
+ifeq ($(PA_BUILD),)
 # Additional settings used in all AOSP builds
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.ringtone=Ring_Synth_04.ogg \
     ro.config.notification_sound=pixiedust.ogg \
+
+endif
 
 # The mainline checking whitelist, should be clean up
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_WHITELIST += \
