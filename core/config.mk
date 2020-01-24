0baf298d52d3392f13640ea4be9bf7103054d89c
Jake Weinstein <jake@aospa.co>
2020-01-24 02:57:46 +0100

    build: include device/pa/sepolicy/qcom on QCOM builds
    Change-Id: I13512dda3c7b4d322f644577a730b404a20ab479

--- core/config.mk
+++ core/config.mk
@@ -1185,6 +1185,11 @@ ifneq ($(wildcard device/pa/sepolicy/common/sepolicy.mk),)
 ## last, to avoid accidental resetting by device configs
 $(eval include device/pa/sepolicy/common/sepolicy.mk)
 endif
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+ifneq ($(wildcard device/pa/sepolicy/qcom/sepolicy.mk),)
+$(eval include device/pa/sepolicy/qcom/sepolicy.mk)
+endif
+endif
 endif
 
 # Include any vendor specific config.mk file
