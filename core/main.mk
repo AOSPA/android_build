0091779b4ca1a16f6e3b42bab45cba258834ef03
Mathieu Chartier <mathieuc@google.com>
2020-06-09 13:52:38 -0700

    Make checkjni only enabled for eng builds
    There is too much performance hit for userdebug builds.
    
    Bug: 156683530
    Test: verify property is not set on aosp_blueline-userdebug
    Change-Id: I5ea442ff4ce54063c1b59b7f661a533dfa73e8f4

--- core/main.mk
+++ core/main.mk
@@ -302,8 +302,6 @@ ifneq (,$(user_variant))
   ADDITIONAL_DEFAULT_PROPERTIES += ro.allow.mock.location=0
 
 else # !user_variant
-  # Turn on checkjni for non-user builds.
-  ADDITIONAL_BUILD_PROPERTIES += ro.kernel.android.checkjni=1
   # Set device insecure for non-user builds.
   ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
   # Allow mock locations by default for non user builds
@@ -323,6 +321,9 @@ endif # !enable_target_debugging
 ## eng ##
 
 ifeq ($(TARGET_BUILD_VARIANT),eng)
+# Turn on checkjni for non-user builds.
+ADDITIONAL_BUILD_PROPERTIES += ro.kernel.android.checkjni=1
+
 tags_to_install := debug eng
 ifneq ($(filter ro.setupwizard.mode=ENABLED, $(call collapse-pairs, $(ADDITIONAL_BUILD_PROPERTIES))),)
   # Don't require the setup wizard on eng builds
