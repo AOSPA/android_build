b2fda9247b27c96ba63d658b4d33480d2fc47429
Kelvin Zhang <zhangkelvin@google.com>
2020-09-22 16:15:57 -0400

    Allow zip64 support when opening zip files
    When opening an zip file through zipfile.ZipFile(), python2 by default
    disables zip64 support. To support update files >4GB, we manually add
    allowZip64 to override the setting.
    
    CRs-Fixed: 2781086
    
    Test: generate && serve an OTA
    
    Change-Id: I9645e963ced830cc2d3a4b72bc63b9369a1cefe8
    (cherry picked from commit 928c2341a6abc3b1ddb7ddd7652e78b67c7ef7a9)

--- tools/releasetools/validate_target_files.py
+++ tools/releasetools/validate_target_files.py
@@ -444,7 +444,7 @@ def main():
   input_tmp = common.UnzipTemp(args.target_files)
 
   info_dict = common.LoadInfoDict(input_tmp)
-  with zipfile.ZipFile(args.target_files, 'r') as input_zip:
+  with zipfile.ZipFile(args.target_files, 'r', allowZip64=True) as input_zip:
     ValidateFileConsistency(input_zip, input_tmp, info_dict)
 
   ValidateInstallRecoveryScript(input_tmp, info_dict)
