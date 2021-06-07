e560ba731b8b36a8456b8e3b7ab273d0df3cfef0
Mohamed Moussa <mmouss@codeaurora.org>
2021-06-07 13:37:02 -0700

    Adding variables for Android.mk
    LOCAL_LDFLAGS_OVERRIDE
    LOCAL_CFLAGS_OVERRIDE
    LOCAL_ASFLAGS_OVERRIDE
    From Android.mk this change will add ability to override global CFI flags included
    by macros: cfiCflags, cfiAsflags and cfiLdflags.
    
    Change-Id: Ia5b5e3314349f5acf88d0290eb4bdb146abd6c7f
    CRs-Fixed: 2964796
    Bug: 185186852

--- core/config_sanitizers.mk
+++ core/config_sanitizers.mk
@@ -428,6 +428,9 @@ ifneq ($(filter cfi,$(my_sanitize)),)
     my_cflags += -fvisibility=default
   endif
   my_ldflags += $(CFI_EXTRA_LDFLAGS)
+  my_ldflags += $(LOCAL_LDFLAGS_OVERRIDE)
+  my_cflags += $(LOCAL_CFLAGS_OVERRIDE)
+  my_asflags += $(LOCAL_ASFLAGS_OVERRIDE)
   my_arflags += --plugin $(LLVM_PREBUILTS_PATH)/../lib64/LLVMgold.so
 
   ifeq ($(LOCAL_FORCE_STATIC_EXECUTABLE),true)
