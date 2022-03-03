afd024bb126acc19c4f81262c8ca135ba8f1961c
Daniel Norman <danielnorman@google.com>
2022-03-03 19:41:13 +0000

    Allow missing key for misc_info lookups.
    This broke the TV-VF build in ab/8246202.
    
    Change-Id: Ifbf76a3c43c46fcd4280180e45ad23fcbed9346d
    (cherry picked from commit c44f964a5ad01d98b50121111998af482b61d79a)

--- tools/releasetools/merge_target_files.py
+++ tools/releasetools/merge_target_files.py
@@ -714,13 +714,13 @@ def merge_meta_files(temp_dir, merged_dir):
       vendor_meta_dir=vendor_meta_dir,
       merged_meta_dir=merged_meta_dir)
 
-  if OPTIONS.merged_misc_info['use_dynamic_partitions'] == 'true':
+  if OPTIONS.merged_misc_info.get('use_dynamic_partitions') == 'true':
     merge_dynamic_partitions_info_txt(
         framework_meta_dir=framework_meta_dir,
         vendor_meta_dir=vendor_meta_dir,
         merged_meta_dir=merged_meta_dir)
 
-  if OPTIONS.merged_misc_info['ab_update'] == 'true':
+  if OPTIONS.merged_misc_info.get('ab_update') == 'true':
     merge_ab_partitions_txt(
         framework_meta_dir=framework_meta_dir,
         vendor_meta_dir=vendor_meta_dir,
