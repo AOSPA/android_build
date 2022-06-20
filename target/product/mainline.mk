b6e83c720dd70dfa3c3a69b62f4310276d27555c
dianlujitao <dianlujitao@lineageos.org>
2022-06-21 02:56:07 +0900

    product: exclude AOSP sounds on AOSPA build
    Change-Id: Ia3ff276f7b926ab5101bc66f8395e46a94b3dd1d
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- target/product/mainline.mk
+++ target/product/mainline.mk
@@ -26,7 +26,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
+ifeq ($(AOSPA_BUILD),)
 $(call inherit-product, frameworks/base/data/sounds/AllAudio.mk)
+endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Ring_Synth_04.ogg \
