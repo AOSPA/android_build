6f6cd634b467a1138b0200004e980ffa5ea0bad5
Rob Seymour <rseymour@google.com>
2022-08-31 23:06:36 +0000

    Update VNDK version to 34.
    Bug: 244449099
    Change-Id: Iaa3538b6103db444a142ef20199ab89044bf0e6c

--- core/version_defaults.mk
+++ core/version_defaults.mk
@@ -84,9 +84,9 @@ PLATFORM_SDK_EXTENSION_VERSION := 3
 # TODO(b/159866756): Remove this workaround when building against
 # BOARD_VNDK_VERSION := current is supported.
 ifeq (true,$(BUILDING_WITH_VSDK))
-  PLATFORM_VNDK_VERSION := S
+  PLATFORM_VNDK_VERSION := U
 else
-  PLATFORM_VNDK_VERSION := 33
+  PLATFORM_VNDK_VERSION := 34
 endif
 
 # This is the sdk extension version that PLATFORM_SDK_VERSION ships with.
