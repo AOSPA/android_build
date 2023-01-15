48551a73663f1af886ce3b984f3069e16070b90e
Simão Gomes Viana <devel@superboring.dev>
2023-01-15 19:19:27 +0100

    Tune javac parameters [1/2]
    Change-Id: Ief4d2779dd0b94f1348ae22ce61cc6faa2f4a625
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- core/combo/javac.mk
+++ core/combo/javac.mk
@@ -16,5 +16,23 @@ ANDROID_JAVA_TOOLCHAIN := $(ANDROID_JAVA_HOME)/bin
 
 # TODO(ccross): remove this, it is needed for now because it is used by
 # config.mk before makevars from soong are loaded
-JAVA := $(ANDROID_JAVA_TOOLCHAIN)/java -XX:OnError="cat hs_err_pid%p.log" -XX:CICompilerCount=2 -XX:+UseDynamicNumberOfGCThreads
-
+JAVA := $(ANDROID_JAVA_TOOLCHAIN)/java -XX:OnError="cat hs_err_pid%p.log" \
+  -XX:CICompilerCount=2 \
+  -XX:+UseG1GC \
+  -XX:+ParallelRefProcEnabled \
+  -XX:MaxGCPauseMillis=500 \
+  -XX:+UnlockExperimentalVMOptions \
+  -XX:+AlwaysPreTouch \
+  -XX:+DisableExplicitGC \
+  -XX:G1NewSizePercent=35 \
+  -XX:G1MaxNewSizePercent=45 \
+  -XX:G1HeapRegionSize=16M \
+  -XX:G1ReservePercent=10 \
+  -XX:G1HeapWastePercent=4 \
+  -XX:G1MixedGCCountTarget=4 \
+  -XX:InitiatingHeapOccupancyPercent=15 \
+  -XX:G1MixedGCLiveThresholdPercent=90 \
+  -XX:G1RSetUpdatingPauseTimePercent=5 \
+  -XX:SurvivorRatio=32 \
+  -XX:+PerfDisableSharedMem \
+  -XX:MaxTenuringThreshold=1
