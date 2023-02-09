b9549be0702b412c826c1e2209d46bb2b64a1ab4
Yi Kong <yikong@google.com>
2023-02-09 15:17:04 +0900

    Migrate to the new clang lib dir
    Test: presubmit
    Change-Id: Ib409725d540d285dfd840bcd3de33020218f3bc0

--- core/clang/config.mk
+++ core/clang/config.mk
@@ -2,8 +2,7 @@
 
 LLVM_READOBJ := $(LLVM_PREBUILTS_BASE)/$(BUILD_OS)-x86/$(LLVM_PREBUILTS_VERSION)/bin/llvm-readobj
 
-# TODO(b/269475377) this path is now out of line with upstream
-LLVM_RTLIB_PATH := $(LLVM_PREBUILTS_BASE)/linux-x86/$(LLVM_PREBUILTS_VERSION)/lib64/clang/$(LLVM_RELEASE_VERSION)/lib/linux/
+LLVM_RTLIB_PATH := $(LLVM_PREBUILTS_BASE)/linux-x86/$(LLVM_PREBUILTS_VERSION)/lib/clang/$(LLVM_RELEASE_VERSION)/lib/linux/
 
 define convert-to-clang-flags
 $(strip $(filter-out $(CLANG_CONFIG_UNKNOWN_CFLAGS),$(1)))
