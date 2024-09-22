34f347dc95c89f56443d016dfc5d341933ccbfa0
Jyotiraditya Panda <jyotiraditya@aospa.co>
2024-09-22 18:15:22 +0530

    core: Add AOSPA specific props
    Change-Id: I801ca1e8fc89cad7956f8371d1a123873139cbc2
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -36,6 +36,12 @@ $(call add_json_str,  BuildHostnameFile,                 build_hostname.txt)
 $(call add_json_str,  BuildThumbprintFile,               build_thumbprint.txt)
 $(call add_json_bool, DisplayBuildNumber,                $(filter true,$(DISPLAY_BUILD_NUMBER)))
 
+$(call add_json_str,  AospaDevice,                       $(TARGET_DEVICE))
+$(call add_json_str,  AospaDisplayVersion,               $(AOSPA_DISPLAY_VERSION))
+$(call add_json_str,  AospaMajorVersion,                 $(AOSPA_MAJOR_VERSION))
+$(call add_json_str,  AospaMinorVersion,                 $(AOSPA_MINOR_VERSION))
+$(call add_json_str,  AospaBuildVariant,                 $(AOSPA_BUILD_VARIANT))
+
 $(call add_json_str,  Platform_display_version_name,     $(PLATFORM_DISPLAY_VERSION))
 $(call add_json_str,  Platform_version_name,             $(PLATFORM_VERSION))
 $(call add_json_val,  Platform_sdk_version,              $(PLATFORM_SDK_VERSION))
